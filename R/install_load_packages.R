a35d804ffbae35059d9ddf9c378ee672e80749e7
camronp <camronpearce@gmail.com>
2025-01-16 10:46:14 -0700

    package dependencies function

---
+++ R/install_load_packages.R
@@ -0,0 +1,21 @@
+
+
+
+install_load_packages <- function() {
+
+  # List of packages required
+  packages <- c("devtools", "ggrepel", "ggforce", "tidyverse",
+                "readxl", "dplyr", "ggplot2", "zoo", "pheatmap", "extrafont", "RColorBrewer")
+
+  # Check and install missing packages
+  new_packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+  if(length(new_packages)) {
+    install.packages(new_packages)
+  }
+
+  # Load packages
+  lapply(packages, library, character.only = TRUE)
+
+  # Return a message or object (if needed)
+  return("All required packages are installed and loaded.")
+}
